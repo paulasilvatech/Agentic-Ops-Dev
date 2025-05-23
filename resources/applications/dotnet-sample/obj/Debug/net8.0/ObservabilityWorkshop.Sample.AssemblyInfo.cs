2e0218d0a7443b52a94d051d3589f909e05e20d2
Paula Silva <paulanunes85@github.com>
2025-05-23 13:32:12 -0300

    chore: Update assembly information and cache files for Observability Workshop sample application

--- resources/applications/dotnet-sample/obj/Debug/net8.0/ObservabilityWorkshop.Sample.AssemblyInfo.cs
+++ resources/applications/dotnet-sample/obj/Debug/net8.0/ObservabilityWorkshop.Sample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObservabilityWorkshop.Sample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115bbbdd18b21a6b319d94e1f32a67a37f4ebeb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37cf6d6a375d1271065d1e41c288f8b403646cb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObservabilityWorkshop.Sample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObservabilityWorkshop.Sample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
